b01b032cb2c70860f503f608098ca090f9c306b7
karnatihemanthreddy <76593875+karnatihemanthreddy@users.noreply.github.com>
2026-01-11 21:07:44 +0530

    Create App.java

---
+++ src/main/java/src/main/java/com/example/app/App.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World 👋");
+        System.out.println("Sum(2, 3) = " + sum(2, 3));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
